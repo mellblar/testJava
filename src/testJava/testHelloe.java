f443796791ecaf6dfff0a8b6a2b2249839fb2224
mlaron <lars.mellberg@tieto.com>
2015-12-16 23:42:04 +0100

    testjava

---
+++ src/testJava/testHelloe.java
@@ -0,0 +1,14 @@
+package testJava;
+
+public class testHelloe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi ");
+
+		
+	     
+	}
+
+}
